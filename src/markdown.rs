f484780a4949a3ca7ce11d410646a0c3f4764c0f
Cem Özden <ozdenc64@gmail.com>
2025-12-27 18:52:53 +0100

    Removing cell

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,5 +1,3 @@
-use std::cell::Cell;
-
 #[derive(Debug, PartialEq, Eq)]
 enum MarkdownError {
     ExhaustedInput,
@@ -13,7 +11,7 @@ enum MarkdownToken {
 }
 
 struct MarkdownParser {
-    offset: Cell<usize>,
+    offset: usize,
     chars: Vec<char>,
 }
 
@@ -22,17 +20,17 @@ impl MarkdownParser {
         let chars: Vec<char> = input.chars().collect();
 
         Self {
-            offset: Cell::new(0),
+            offset: 0,
             chars,
         }
     }
 
-    pub fn parse(&self) -> Result<Vec<MarkdownToken>, MarkdownError> {
+    pub fn parse(&mut self) -> Result<Vec<MarkdownToken>, MarkdownError> {
         let chars_len = self.chars.len();
         let mut result = Vec::with_capacity(10);
 
-        while self.offset.get() < chars_len {
-            let ch = self.chars[self.offset.get()];
+        while self.offset < chars_len {
+            let ch = self.chars[self.offset];
 
             match ch {
                 '#' => result.push(self.parse_header_or_text()?),
@@ -44,39 +42,38 @@ impl MarkdownParser {
         Ok(result)
     }
 
-    fn parse_header_or_text(&self) -> Result<MarkdownToken, MarkdownError> {
+    fn parse_header_or_text(&mut self) -> Result<MarkdownToken, MarkdownError> {
         // Increase offset so that we can switch to the next character
-        self.increase_offset();
+        self.offset += 1;
 
-        if self.offset.get() == self.chars.len() {
+        if self.offset == self.chars.len() {
             return Err(MarkdownError::ExhaustedInput);
         }
 
-        let current_ch = self.chars[self.offset.get()];
+        let current_ch = self.chars[self.offset];
 
         if current_ch == ' ' {
-            self.increase_offset();
+            self.offset += 1;
             self.parse_header()
         } else {
             todo!("Text parsing will be done later")
         }
     }
 
-    fn parse_header(&self) -> Result<MarkdownToken, MarkdownError> {
+    fn parse_header(&mut self) -> Result<MarkdownToken, MarkdownError> {
         let mut header_chars = Vec::with_capacity(30);
 
-        while self.offset.get() < self.chars.len() {
-            let ch = self.chars[self.offset.get()];
+        while self.offset < self.chars.len() {
+            let ch = self.chars[self.offset];
 
             if ch == '\n' {
-                self.offset.set(self.offset.get());
                 let header_text: String = header_chars.into_iter().collect();
 
                 return Ok(MarkdownToken::Header1(header_text));
             }
 
             header_chars.push(ch);
-            self.increase_offset();
+            self.offset += 1;
         }
 
         let header_text: String = header_chars.into_iter().collect();
@@ -84,16 +81,12 @@ impl MarkdownParser {
         Ok(MarkdownToken::Header1(header_text))
     }
 
-    fn parse_new_line(&self) -> MarkdownToken {
-        self.increase_offset();
+    fn parse_new_line(&mut self) -> MarkdownToken {
+        self.offset += 1;
 
         MarkdownToken::NewLine
     }
 
-    #[inline]
-    fn increase_offset(&self) {
-        self.offset.set(self.offset.get() + 1);
-    }
 }
 
 #[cfg(test)]
@@ -105,7 +98,7 @@ mod tests {
     fn parse_header_1() {
         let input = "# Hello World!";
 
-        let markdown_parser = MarkdownParser::new(input);
+        let mut markdown_parser = MarkdownParser::new(input);
         let result = markdown_parser.parse().unwrap();
 
         assert_eq!(
@@ -118,7 +111,7 @@ mod tests {
     fn parse_header_1_with_newline() {
         let input = "# Hello World!\n";
 
-        let markdown_parser = MarkdownParser::new(input);
+        let mut markdown_parser = MarkdownParser::new(input);
         let result = markdown_parser.parse().unwrap();
 
         assert_eq!(
